2314bbac16b6ad5371e10fdcb568cedba1f98989
Nicolás Vásquez Murcia <n.vasquez10@uniandes.edu.co>
2017-03-29 22:44:08 -0500

    Button Title

--- imports/ui/components/programs/Program.jsx
+++ imports/ui/components/programs/Program.jsx
@@ -5,7 +5,10 @@ import {Meteor} from "meteor/meteor";
 import {Link} from 'react-router';
 // import Courses from '../courses/courses';
 
-
+/*Seria bueno poner un title o un tag mas facil de entender para el boton que redirige a la pagina del depto para guiarse mas facil.
+Esto mismo para cada clase del programa, de resto todo esta muy bien en cuanto a usabilidad. Iba a sugerir cambiar la descripcion 
+para poner en negrilla valores como SNIES.. etc, pero veo que es una sola descripcion por programa en la base de datos y queda complicado.
+*/
 class Program extends Component {
 
   render(){
@@ -23,6 +26,7 @@ class Program extends Component {
             {/* <button className="btn btn-danger" onClick={this.deleteProgram.bind(this)} >
              Delete
             </button> */}
+            
             <a href={this.props.program.url} target="_blank" className="btn btn-info cardbtn"><i className="fa fa-fw fa-globe"></i></a>
 
             <Link className="btn btn-primary " to={'/programs/' + this.props.program._id + '/courses'}>Courses</Link>
