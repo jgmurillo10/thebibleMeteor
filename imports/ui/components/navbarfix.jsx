8074e239c987119c7962ca9cfac615ea728e07a8
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-03-31 07:34:12 -0500

    deleted unfunctionality navbar buttons

--- imports/ui/components/navbarfix.jsx
+++ imports/ui/components/navbarfix.jsx
@@ -49,8 +49,6 @@ class Navbarfix extends Component {
               {!loggedIn?
                   <ul className="nav navbar-nav">
                   <li><a href="#">Home</a></li>
-                  <li><a href="#about">About</a></li>
-                  <li><a href="#contact">Contact</a></li>
                   </ul>
                  : ''
               }
