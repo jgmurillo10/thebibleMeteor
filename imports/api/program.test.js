f877ef6b918d4e403d6530858c877d73f66fad0d
María Arévalo <mi.arevalo10@uniandes.edu.co>
2017-04-18 16:07:39 -0500

    test methods

--- imports/api/program.test.js
+++ imports/api/program.test.js
@@ -6,10 +6,38 @@ import { Factory } from 'meteor/dburles:factory';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 import faker from 'faker';
 import {ProgramsMongo} from './programs.js';
+import StubCollections from 'meteor/hwillson:stub-collections';
+import { Random } from 'meteor/random';
+
+
+StubCollections.stub(ProgramsMongo);
 
 Factory.define('programtest', ProgramsMongo, {
 
   name:()=>'ISIS',
   description:()=> 'testing',
   url: ()=>'google.com'
+});
+describe('Programs', () => {
+  describe('add program', function () {
+    beforeEach(function () {
+      resetDatabase();
+    });
+    it('should add a program', function () {
+      const pname = faker.lorem.word;
+      const pdescription =faker.lorem.sentence();
+      const purl = faker.internet.url;
+
+      Meteor.call('programs.add', {
+       name: pname,
+       description: pdescrition,
+       url: purl;
+       }
+
+       chai.assert.equal(ProgramsMongo.find().count(), 1);
+
+
+       })
+    });
+  });
 });
