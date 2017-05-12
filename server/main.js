bb6db8c81c1e3e81d4187b9f1b544d771e2da4d2
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-05-12 16:52:32 -0500

    adding homepagepage

--- server/main.js
+++ server/main.js
@@ -2,18 +2,4 @@ import { Meteor } from 'meteor/meteor';
 import { Programs } from '../imports/api/programs';
 
 Meteor.startup(() => {
-  // code to run on server at startup
-  //BrowserPolicy.content.disallowInlineScripts();
-  //BrowserPolicy.content.allowInlineStyles();
-  if(BrowserPolicy){
-BrowserPolicy.content.allowOriginForAll('*.bootstrapcdn.com');
-BrowserPolicy.content.allowOriginForAll('*.googleapis.com');
-BrowserPolicy.content.allowOriginForAll('*.rawgit.com');
-BrowserPolicy.content.allowOriginForAll('*.jquery.com');
-BrowserPolicy.content.allowOriginForAll('*.hotjar.com');
-BrowserPolicy.content.allowOriginForAll('*.gstatic.com');
-BrowserPolicy.content.allowDataUrlForAll();
-  }
-
-
 });
