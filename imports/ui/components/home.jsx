dc5ffb9e58a1ef7daa67a6248df99918d7a34f70
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-05-13 08:46:38 -0500

    homepage done, demo

--- imports/ui/components/home.jsx
+++ imports/ui/components/home.jsx
@@ -30,14 +30,14 @@ class Home extends Component {
           <section id="about" className="about content-section alt-bg-light">
               <div className="container">
                   <div className="row">
-                      <div className="col-md-6">
+                      <div className="col-md-6 col-xs-12">
                           <h2>Bienvenidos</h2>
                           <h3 className="caption gray">Carga tus archivos desde donde quieras</h3>
                           <p>TheBibleApp es una iniciativa de estudiantes, para estudiantes. Esta aplicación te permite subir apuntes, talleres, solucionarios y demás documentos útiles para la vida universitaria.</p>
 
                           <blockquote>
-                              There are two types of people who will tell you that you cannot make a difference in this world: those who are afraid to try and those who are afraid you will succeed.
-                              <span>Ray Goforth</span>
+                              Internet facilita la información adecuada, en el momento adecuado, para el propósito adecuado.
+                              <span>Bill Gates</span>
                           </blockquote>
 
                           <h3 className="gray light">Este proyecto es una aplicación web diseñada para almacenar archivos académicos. Los archivos están organizados por programa y curso de la Universidad de los Andes.
@@ -47,10 +47,10 @@ Nota: evite cargar archivos confidenciales. Estos archivos se almacenan en Amazo
 
                       </div>
 
-                      <div className="col-md-6">
+                      <div className="col-md-6 col-xs-12">
 
                           <div className="embed-responsive embed-responsive-16by9">
-                              <iframe src="https://player.vimeo.com/video/132613118?title=0&amp;byline=0&amp;portrait=1" className="embed-responsive-item" frameBorder="0" webkitallowfullscreen mozAllowFullScreen allowFullScreen></iframe>
+                              <iframe src="https://player.vimeo.com/video/132613118?title=0&amp;byline=0&amp;portrait=1" className="embed-responsive-item" frameBorder="0"  allowFullScreen></iframe>
                           </div>
                       </div>
                   </div>
@@ -70,7 +70,7 @@ Nota: evite cargar archivos confidenciales. Estos archivos se almacenan en Amazo
                           <div className="row text-center">
                               <div className="col-md-4">
                                   <div className="row services-item sans-shadow text-center">
-                                      <i className="fa fa-cogs fa-3x"></i>
+                                      <i className="fa fa-share-alt fa-3x"></i>
                                       <h4>Compartir</h4>
                                       <p>Subir archivos de direntes materias de diferentes programas.</p>
                                   </div>
@@ -78,7 +78,7 @@ Nota: evite cargar archivos confidenciales. Estos archivos se almacenan en Amazo
 
                               <div className="col-md-4">
                                   <div className="row services-item sans-shadow text-center">
-                                      <i className="fa fa-paint-brush fa-3x"></i>
+                                      <i className="fa fa-cloud-download fa-3x"></i>
                                       <h4>Descargar</h4>
                                       <p>Cientos de talleres, solucionarios, explicaciones y demás material disponible.</p>
                                   </div>
@@ -86,7 +86,7 @@ Nota: evite cargar archivos confidenciales. Estos archivos se almacenan en Amazo
 
                               <div className="col-md-4">
                                   <div className="row services-item sans-shadow text-center">
-                                      <i className="fa fa-bullhorn fa-3x"></i>
+                                      <i className="fa fa-refresh fa-3x"></i>
                                       <h4>Actualizar</h4>
                                       <p>Actualizar o corregir material publicado en la aplicación.</p>
                                   </div>
@@ -94,7 +94,7 @@ Nota: evite cargar archivos confidenciales. Estos archivos se almacenan en Amazo
 
                               <div className="col-md-4">
                                   <div className="row services-item sans-shadow text-center">
-                                      <i className="fa fa-database fa-3x"></i>
+                                      <i className="fa fa-cogs fa-3x"></i>
                                       <h4>Fortalecer</h4>
                                       <p>Fortalecer tus conocimientos en la materia que no vas tan bien.</p>
                                   </div>
@@ -102,7 +102,7 @@ Nota: evite cargar archivos confidenciales. Estos archivos se almacenan en Amazo
 
                               <div className="col-md-4">
                                   <div className="row services-item sans-shadow text-center">
-                                      <i className="fa fa-align-left fa-3x"></i>
+                                      <i className="fa fa-pencil fa-3x"></i>
                                       <h4>Preparar</h4>
                                       <p>Prepararte para parciales, finales, quices e incluso para participar en clase.</p>
                                   </div>
@@ -110,7 +110,7 @@ Nota: evite cargar archivos confidenciales. Estos archivos se almacenan en Amazo
 
                               <div className="col-md-4">
                                   <div className="row services-item sans-shadow text-center">
-                                      <i className="fa fa-camera fa-3x"></i>
+                                      <i className="fa fa-cubes fa-3x"></i>
                                       <h4>Entender</h4>
                                       <p>Entender a fondo los temas de esa materia en la que no entiendes nada.</p>
                                   </div>
